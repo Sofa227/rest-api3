0981c45edd595656038f905e8c20dbe643bcbbd7
Sofa227 <125588122+Sofa227@users.noreply.github.com>
2024-03-31 22:57:03 +0300

    Delete Marina directory

--- Marina/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-	$('.hide-password').on('click', function(){
-		var $this= $(this),
-			$password_field = $this.prev('input');
-		
-		( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
-		( 'Скрыть' == $this.text() ) ? $this.text('Показать') : $this.text('Скрыть');
-	});
-});
--- Marina/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_unset();
-include('main_window.php');
-?>
